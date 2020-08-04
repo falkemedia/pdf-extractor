69666053fd1570ef4786aa48c4a914faed11062c
Robin Reiter <robin7331@gmail.com>
2020-08-04 08:03:50 +0200

    basic structure created

---
+++ src/Extractor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Robin7331\PdfExtractor;
+
+class Extractor
+{
+
+    public static function test()
+    {
+        return "hi";
+    }
+}
---
+++ src/examples/index.php
@@ -0,0 +1,8 @@
+<?php
+namespace Robin7331\PdfExtractor\Examples;
+
+require 'vendor/autoload.php';
+
+use Robin7331\PdfExtractor\Extractor;
+
+echo Extractor::test();
